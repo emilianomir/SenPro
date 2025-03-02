09e2d68253a77ea1c2f5c718d31c64ce70224346
TheHiddenError <marcos48201@gmail.com>
2025-03-02 09:45:39 -0600

    Forgot file from merge

--- src/app/services/[service_info]/page.js
+++ src/app/services/[service_info]/page.js
@@ -8,11 +8,7 @@ import { useEffect } from "react";
 
 
 export default function ServiceInfo(){
-<<<<<<< HEAD
     const {userServices, setServices} = useAppContext();
-=======
-    const [userServices, changeService] = useAppContext();
->>>>>>> origin/main
     const router = useRouter();
 
     const handleBack = ()=>{
@@ -21,11 +17,7 @@ export default function ServiceInfo(){
 
     useEffect(() => {
         const handleRouteChangeComplete = () => {
-<<<<<<< HEAD
           setServices(null);
-=======
-          setService(null);
->>>>>>> origin/main
         };
     }
     )
