1adaf5d68def1afd5447aa01ff0c3000388f72ca
GaelGalvan <112572380+GaelGalvan@users.noreply.github.com>
2025-03-02 23:10:51 +0000

    bcrypt testing

--- src/db/index.js
+++ src/db/index.js
@@ -6,6 +6,6 @@ config({ path: '.env.local' }); // or .env.local
 
 // Making use of local Evironments doesn't seem to work ( later fix)
 export const db = drizzle({ connection: {
-  url: process.env.TURSO_CONNECTION_URL, // change this
-  authToken: process.env.TURSO_AUTH_TOKEN, // change this
+  url: 'libsql://servicesapp-gaelgalvan.turso.io', // change this
+  authToken: 'eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3NDA5NTA1ODksImlkIjoiMmFlOTE4NzYtMTdlMC00ZDIzLWFmMWUtNDYwZGMyNWMxOWE2In0.Z9GMj5TeiqcS72v_V_SWDDQ9HTipuPu6dcvYfJnMZZucp89ZAJUWad4DYP_gJoT6GUWp9eOOP5Xw8GQqoVJzAg', // change this
 }});
--- src/db/testing.js
+++ src/db/testing.js
@@ -2,21 +2,25 @@
 import { db } from "./index.js";
 import { users } from "./schema/users.js";
 import { eq } from "drizzle-orm";
-import { addUser, testExistingUser} from "/workspaces/SenPro/src/components/DBactions.js";
+import { addUser, checkLogin, testExistingUser, getUser} from "/workspaces/SenPro/src/components/DBactions.js";
 // import { addUser, testExistingUser } from "@/components/DBactions.js";
+import bcrypt from "bcrypt"
+import { getModifiedCookieValues } from "next/dist/server/web/spec-extension/adapters/request-cookies.js";
 
 /*
-return db.insert( users ).values({
+await db.insert( users ).values({
     email: "gaelg@gmail.com",
     username: "gaelg",
     password: "testingpass",
     type: "admin",
     address: "78574",
 })
-}
 */
 
-//addUser("testEmail@gmail.com", "testuseer", "password");
+
+
+
+//addUser("gaelg@gmail.com", "gaelg", "testingpass", "78574", "admin");
 testExistingUser("gaelg@gmail.com").then((data) =>
 {
     if(data)
@@ -25,5 +29,20 @@ testExistingUser("gaelg@gmail.com").then((data) =>
         console.log("This is false")
 }) // get past awaits
  
+
+checkLogin("gaelg@gmail.com", "testingpass").then((data) =>
+    {
+        if(data)
+            console.log("This is true")
+        else
+            console.log("This is false")
+    }) // get past awaits
+
+
+
+const values = await getUser("gaelg@gmail.com")
+console.log(values[0].username);
+
 const test = await db.select({email: users.email}).from(users).where(eq(users.email, 'gaelg@gmail.com')) 
 //console.log(test.length)
+
