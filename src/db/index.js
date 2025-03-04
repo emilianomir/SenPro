238317c912f868eacc12fd18fcf57b859912929f
GaelGalvan <112572380+GaelGalvan@users.noreply.github.com>
2025-03-04 05:29:05 +0000

    Pending changes exported from your codespace

--- src/db/index.js
+++ src/db/index.js
@@ -8,8 +8,8 @@ const connectionUrl = process.env.TURSO_CONNECTION_URL;
 const authToken = process.env.TURSO_AUTH_TOKEN;
 
 const client = createClient({
-  url: connectionUrl,
-  authToken: authToken,
+  url: 'libsql://servicesapp-gaelgalvan.turso.io',
+  authToken: 'eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3NDA5NTA1ODksImlkIjoiMmFlOTE4NzYtMTdlMC00ZDIzLWFmMWUtNDYwZGMyNWMxOWE2In0.Z9GMj5TeiqcS72v_V_SWDDQ9HTipuPu6dcvYfJnMZZucp89ZAJUWad4DYP_gJoT6GUWp9eOOP5Xw8GQqoVJzAg',
 });
 
 export const db = drizzle(client);
