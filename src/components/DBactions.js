790eecf77c1d64f75e276182ae3714941fbb1d05
Emiliano <emirando117@gmail.com>
2025-03-06 19:03:33 -0600

    rebase: more merging conflicts

--- src/components/DBactions.js
+++ src/components/DBactions.js
@@ -4,7 +4,6 @@ import { users } from "../db/schema/users.js";
 import { eq } from "drizzle-orm";
 import { questions } from "../db/schema/questions.js";
 
-
 // testing for existing emails in singup
 export async function testExistingUser(email){
    const data = await db.select().from(users).where(eq(users.email, email));
@@ -16,18 +15,32 @@ export async function testExistingUser(email){
    {
     return true
    }
+// testing for existing emails
+export async function testExistingUser(email) {
+  const data = await db.select().from(users).where(eq(users.email, email));
+  if (data.length === 0) {
+    return false;
+  } else {
+    return true;
+  }
 }
- 
 
 // Adding User for Sign up
-export async function addUser (email, username, password, address = '', type = 'user'){
-    await db.insert( users ).values({
-        email: email,
-        username: username,
-        password: password,
-        address: address,
-        type: type,
-    })
+export async function addUser(
+  email,
+  username,
+  password,
+  address = "",
+  type = "user"
+) {
+  const pass = await bcrypt.hash(password, 10);
+  await db.insert(users).values({
+    email: email,
+    username: username,
+    password: password,
+    address: address,
+    type: type,
+  });
 }
 
 
@@ -45,37 +58,39 @@ export async function checkLogin(email, password){
     }
  }
 export async function updateUserAddress(email, address) {
-    try {
-        await db.update(users)
-            .set({ address: address })
-            .where(eq(users.email, email));
-        console.log("address updated successfully");
-    } catch (e) {
-        console.error("error updating address:", e);
-        throw e;
-    }
+  try {
+    await db
+      .update(users)
+      .set({ address: address })
+      .where(eq(users.email, email));
+    console.log("address updated successfully");
+  } catch (e) {
+    console.error("error updating address:", e);
+    throw e;
+  }
 }
 
 export async function addQuestion(userEmail, question, answer) {
-    try {
-        console.log("adding question to db:", {
-            userEmail,
-            question,
-            answer
-        });
-        
-        await db.insert(questions).values({
-            userEmail: userEmail,
-            question: question,
-            answer: answer
-        });
-    } catch (e) {
-        console.error("error in in question adding:", e);
-        throw e;
-    }
+  try {
+    console.log("adding question to db:", {
+      userEmail,
+      question,
+      answer,
+    });
+
+    await db.insert(questions).values({
+      userEmail: userEmail,
+      question: question,
+      answer: answer,
+    });
+  } catch (e) {
+    console.error("error in in question adding:", e);
+    throw e;
+  }
 }
 
 export async function getUserQuestions(userEmail) {
+<<<<<<< HEAD
     try {
         const data = await db.select().from(questions).where(eq(questions.userEmail, userEmail));
         return data;
@@ -83,4 +98,37 @@ export async function getUserQuestions(userEmail) {
         console.error("error getting questions and answers:", e);
         throw e;
     }
+=======
+  try {
+    const data = await db
+      .select()
+      .from(questions)
+      .where(eq(questions.userEmail, userEmail));
+    return data;
+  } catch (e) {
+    console.error("error getting questions and answers:", e);
+    throw e;
+  }
+}
+
+export async function checkLogin(email, password) {
+  const data = await db.select().from(users).where(eq(users.email, email));
+  if (data.length === 0) {
+    return false;
+  } else {
+    return await bcrypt.compare(password, data[0].password);
+  }
+}
+
+// get user attrributes
+export async function getUser(email) {
+  return await db
+    .select({
+      email: users.email,
+      username: users.username,
+      address: users.address,
+    })
+    .from(users)
+    .where(eq(users.email, email));
+>>>>>>> 8f25892 (add bycript)
 }
--- src/db/schema/questions.js
+++ src/db/schema/questions.js
@@ -3,9 +3,11 @@ import { integer, text, sqliteTable } from "drizzle-orm/sqlite-core";
 import { users } from "./users.js"; // reference to users table so we can store user email
 
 export const questions = sqliteTable("questions", {
-    id: integer("id").primaryKey({ autoIncrement: true }),
-    userEmail: text("user_email").notNull().references(() => users.email),
-    question: text("question").notNull(),
-    answer: text("answer").notNull(),
-    createdAt: integer("created_at").default(sql`(CURRENT_TIMESTAMP)`)
+  id: integer("id").primaryKey({ autoIncrement: true }),
+  userEmail: text("user_email")
+    .notNull()
+    .references(() => users.email),
+  question: text("question").notNull(),
+  answer: text("answer").notNull(),
+  createdAt: integer("created_at").default(sql`(CURRENT_TIMESTAMP)`),
 });
