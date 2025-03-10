c825db52a1d072a855ded45172bfc5901b84100d
TheHiddenError <marcos48201@gmail.com>
2025-03-10 17:57:38 -0500

    Small display change in favorites

--- src/components/Favorites_Section.js
+++ src/components/Favorites_Section.js
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
 import "../app/css/services_page.css"
-=======
->>>>>>> origin/main
 export default function Favorites_Section ({favoritesList}){
 
     return (
-        <div className="container">
+        <div className="container pb-5">
             {favoritesList.length === 0 ? 
             <div className="text-center fs-4 text-white">
                 No Favorites Yet. Favorite your favorite services to see them here!
@@ -13,16 +10,12 @@ export default function Favorites_Section ({favoritesList}){
             :
             <div className="scroll">
                 {favoritesList.map((theFavorite) => (
-<<<<<<< HEAD
-                    <div className="d-inline-block me-4">
-                        <div key = {theFavorite.name} className="card">
+                    <div key = {theFavorite.name} className="d-inline-block me-4">
+                        <div className="card">
                             <img src = {theFavorite.photo} className="card-img-top"/>
                             <h5 className="card-title fs-3 text-center fw-bold">{theFavorite.name}</h5>
                         </div>
                     </div>
-=======
-                    <div className="bg-white"> {theFavorite.name} </div>
->>>>>>> origin/main
                 ) )
                 }
             </div>
