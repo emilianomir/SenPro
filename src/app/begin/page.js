8bb99580bcaa6d655b6859ed9e9af3168eaa9589
TheHiddenError <marcos48201@gmail.com>
2025-03-09 20:00:44 -0500

    Begin page added

---
+++ src/app/begin/page.js
@@ -0,0 +1,45 @@
+"use client"
+import "../css/begin_page.css"
+import { useAppContext } from "@/context"
+import { redirect, useRouter } from "next/navigation";
+import RouteButton from "@/components/route_button";
+
+export default function Begin(){
+    const {userEmail} = useAppContext();
+    const hasAccount = userEmail != null ? true: false;
+    let userName;
+    if (!hasAccount)
+        redirect("/login");
+    else {
+        userName = userEmail.split("@")[0].toUpperCase();
+    }
+    return (
+        <div className="container mt-4">
+            <div className="mt-5">
+                <h1 className="fs-1 text-white text-center fw-bolder">Welcome {userName} </h1>
+                <div className="fs-2 text-white text-center mt-3 mb-5">What are you planning to do today?</div>
+            </div>
+            <div className="container mt-4 squares">
+                <div className="row row-col-2 h-100 mt-5">
+                    <div className="col w-100 h-75 bg-secondary-subtle me-4">
+                        <h2 className="text-center fw-bolder mt-3 pt-4 fs-1">Plan Trip</h2>
+                        <div className="d-flex justify-content-center mt-5"> 
+                            <div className="w-50">
+                                <RouteButton name = {"GO!"} location = {"/start"} />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col w-100 h-75 bg-secondary-subtle">
+                        <h2 className="text-center fw-bolder mt-3 pt-4 fs-1">View Past Trips</h2>
+                        <div className="d-flex justify-content-center mt-5"> 
+                            <div className="w-50">
+                                <RouteButton name = {"GO!"} location = {"/history"} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
--- src/app/login/page.js
+++ src/app/login/page.js
@@ -6,8 +6,10 @@ import "../css/login_page.css";
 import RouteButton from "@/components/route_button";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { useAppContext } from "@/context";
 
 function LoginPage() {
+  const {setUserEmail} = useAppContext();
   const router = useRouter();
   const [formData, changeFormData] = useState({
     inputEmail: "",
@@ -26,35 +28,14 @@ function LoginPage() {
     const submitForm = (event) => {
       event.preventDefault();
 
-
-      const submitForm = (event) => {
-        event.preventDefault();
-
-        if (!formData.inputEmail || !formData.inputPass) {
-          alert("Please fill out all fields");
-          return;
-        }
-
-
-        // Checking Login Credentials
-        checkLogin(formData.inputEmail, formData.inputPass).then((data) => {
-          if (!data) {
-            alert("Invalid email or pass");
-            return;
-          } else {
-            router.push("/start?user=" + formData.inputEmail);
-          }
-        });
-      };
-
-
       // Checking Login Credentials
       checkLogin(formData.inputEmail, formData.inputPass).then((data) => {
         if (!data) {
           alert("Invalid email or pass");
           return;
         } else {
-          router.push("/start?user=" + formData.inputEmail);
+          setUserEmail(formData.inputEmail);
+          router.push("/begin");
         }
       });
     };
