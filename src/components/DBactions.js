89a243ab5e00845643be79d574d835e55903df70
GaelGalvan <gael.galvan01@utrgv.edu>
2025-04-16 19:19:36 -0500

    small history fixes: more to be changed

--- src/components/DBactions.js
+++ src/components/DBactions.js
@@ -375,12 +375,16 @@ export async function selectHistory(email)
   const val1 = await db.select({services: history.sAddress, date: history.createdAt }).from(history).where(eq(history.userEmail, email));
   for (const element of val1){
       const val2 = JSON.parse(element.services)
+      // Used to Call API
+      
+    
       const val3 = [];
       for(const element of val2){
           let service = await db.select({ info: services.address } ).from(services).where(eq(services.address, element));
           service =  await getAPI(service[0].info);
           val3.push(service);
       }
+  
       let valMap = { "date": new Date (element.date), "services": val3 };
       fullArray.push(valMap);
   }
