b5ae4b3c5066f4617d04a2db7f28ee6ae5284a37
Emiliano <emirando117@gmail.com>
2025-05-07 10:24:37 -0500

    use id for the object

--- src/utils/loadGoogleMaps.js
+++ src/utils/loadGoogleMaps.js
@@ -2,15 +2,27 @@
 // it is used to load the google maps script and then call the callback function
 //basically it like a pseduo map api, so we can mock the map api
 export function loadGoogleMapsScript(callback) { // cb is initMap
-    if (typeof window === 'undefined' || window.google) { // no window or google object already load
-      callback(); // so run the cb
+    if (typeof window === 'undefined') { // if no window, return
       return;
     }
-    // mock the script
+
+    // script already loaded, so run the callback
+    const existing = document.getElementById('google-maps'); // check if the script is already loaded via the id
+    if (existing) {
+      if (window.google && window.google.maps) {
+        callback(); // if the script is already loaded, run the callback
+      } else {
+        existing.addEventListener('load', callback); // if the script is not loaded, add an event listener to the script
+      }
+      return;
+    }
+
+    // first time, inject the script
     const script = document.createElement('script');
-    script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}&libraries=places`;
-    script.async = true;
-    script.defer = true;
+    script.id = 'google-maps'; // give the script an id to be able to check if it is loaded
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}&libraries=places`; // inject the script
+    script.async = true; // async to not block the page
+    script.defer = true;// same
     script.onload = callback;
-    document.head.appendChild(script);
+    document.head.appendChild(script); // head is where scripts go in html/dom
   }
